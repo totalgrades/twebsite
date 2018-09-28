0072deb8a0e9c828f99f3c6e041383d4f27d37b7
nahorr <nahorr@gmail.com>
2018-09-28 14:34:06 -0600

    started replies

--- resources/views/documentation/alldocumentation.blade.php
+++ resources/views/documentation/alldocumentation.blade.php
@@ -51,7 +51,7 @@
 
 									  		<li class="list-group-item d-flex justify-content-between align-items-center">
 									  			<a href="{{ url('documentation/showdocumentation/'.$post->id) }}"> {{$post->post_title}}</a>
-									  			<span class="badge badge-primary badge-pill">{{$post->post_comments}} comments</span>
+									  			<span class="badge badge-primary badge-pill">{{$post->comments->count()}} comments</span>
 									  		</li>
 									  	@endforeach
 									</ul>
